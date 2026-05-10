11592323ad77013165b0ad36f2d6aa6dd2c78b60
etools-ammad <ahadi@e-tools.com.au>
2026-05-10 19:22:58 +0500

    Card Game Project convert into node js based. Git initialization

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const outputDir = "./dist";
+
+module.exports = {
+    entry: path.resolve(__dirname, "src", "index.js"), //
+    output: {
+        path: path.join(__dirname, outputDir),
+        filename: "pokerOdds.js",
+        publicPath: "/dist/"
+    },
+    resolve: {
+        extensions: [".js"] 
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: { presets: ["env"] } 
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: ["css-loader", "postcss-loader"],
+                    fallback: "style-loader"
+                })
+            }
+        ]
+    },
+    plugins: [new ExtractTextPlugin("[name].css"), require("autoprefixer")]
+};
